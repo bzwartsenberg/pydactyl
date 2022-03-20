47ce76d38752e3ec251025d68c0646d581b1b083
Berend Zwartsenberg <berend@zwartsenberg.nl>
2022-03-20 16:36:02 -0700

    Some code

---
+++ main.py
@@ -0,0 +1,13 @@
+import argparse
+from solid import union, difference, cube, sphere
+
+
+class Keyboard():
+
+    def __init__(self, args):
+
+        self.args = args
+
+    def single_keyhole(self):
+
+        return keyhole
