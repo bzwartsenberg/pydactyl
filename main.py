322b34492c091d715f54ff81f154e66ab7b56862
Berend Zwartsenberg <berend@zwartsenberg.nl>
2022-04-03 12:07:45 -0700

    Added more objects to solid update

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import argparse
-from solid import union, difference, cube, sphere, scad_render_to_file, translate, mirror, rotate, hull, cylinder
+from solid_update import Cube, Cylinder, Sphere
+from solid_update import Union, Difference, Intersection, Hull
+from solid_update import Translate, Mirror, Scale, Rotate
+from solid import scad_render_to_file
 import sys
+import numpy as np
 
 # def cube(x, y=None, z=None):
 #     if y is None:
+#
+def rotate_around_origin(shape, origin, angle, axis):
+    shape = Translate([-v for v in origin])(shape)
+    shape = Rotate(angle, axis)(shape)
+    shape = Translate(origin)(shape)
+
+    return shape
 
 class Keyboard():
 
@@ -13,7 +24,7 @@ class Keyboard():
 
     def single_keyhole(self):
 
-        keyhole = cube(10)
+        keyhole = Cube(10)
 
         # some shortcuts
         kr = 1.5 #TODO make parameter
@@ -22,23 +33,23 @@ class Keyboard():
         pt = self.args.plate_thickness
 
         #top wall
-        translation = translate([0., kr / 2 + kh / 2, pt / 2])
-        top_wall = translation(cube([kw + kr * 2, kr, pt], center=True))
+        translation = Translate([0., kr / 2 + kh / 2, pt / 2])
+        top_wall = translation(Cube([kw + kr * 2, kr, pt], center=True))
 
         #side wall:
-        translation = translate([kr / 2 + kw / 2, 0., pt / 2])
-        left_wall = translation(cube([kr, kh + kr * 2, pt], center=True))
+        translation = Translate([kr / 2 + kw / 2, 0., pt / 2])
+        left_wall = translation(Cube([kr, kh + kr * 2, pt], center=True))
 
         #side-nub:
         rt = self.args.retention_tab_thickness
         st = self.args.side_nub_thickness
         rht = pt - rt
         nw = 2.75  # nub width
-        rotation = rotate(90, [1, 0, 0])
-        translation = translate([kw /2, 0, 0])
-        partial_side_nub_1 = rotation(translation(cylinder(1.0, nw, center=True, segments=30)))
-        partial_side_nub_2 = translate([kr / 2 + kw / 2, 0, st / 2])(cube([kr, nw, st], center=True))
-        side_nub = translate([0., 0., pt - st])(hull()(partial_side_nub_1, partial_side_nub_2))
+        rotation = Rotate(90, [1, 0, 0])
+        translation = Translate([kw /2, 0, 0])
+        partial_side_nub_1 = rotation(translation(Cylinder(1.0, nw, center=True, segments=30)))
+        partial_side_nub_2 = Translate([kr / 2 + kw / 2, 0, st / 2])(Cube([kr, nw, st], center=True))
+        side_nub = Translate([0., 0., pt - st])(Hull()(partial_side_nub_1, partial_side_nub_2))
 
         plate_half = left_wall + top_wall
 
@@ -46,18 +57,57 @@ class Keyboard():
             plate_half = plate_half + side_nub
 
 
-        top_nub = translate([kw / 2, 0, rht / 2])(cube([5., 5., rht], center=True))
-        top_nub_pair = top_nub + mirror([0, 1, 0])(mirror([1, 0, 0])(top_nub))
+        top_nub = Translate([kw / 2, 0, rht / 2])(Cube([5., 5., rht], center=True))
+        top_nub_pair = top_nub + Mirror([0, 1, 0])(Mirror([1, 0, 0])(top_nub))
 
-        plate = plate_half + mirror([0, 1, 0])(mirror([1, 0, 0])(plate_half))
-        plate = plate - rotate(90, [0, 0, 1])(top_nub_pair)
+        plate = plate_half + Mirror([0, 1, 0])(Mirror([1, 0, 0])(plate_half))
+        plate = plate - Rotate(90, [0, 0, 1])(top_nub_pair)
 
         return plate
+
+    def switch_cutout(self):
+        kr = 1.5 # TODO: merge with other parameter
+        return Cube([self.args.keyswitch_width + 2 * kr, self.args.keyswitch_height + 2 * kr, 50.], center=True)
+
+    def transform_switch(self, shape, row, col):
+        cap_top_height = self.args.plate_thickness + self.args.key_height
+        mh = self.args.keyswitch_height + 3.0
+
+        rr = (self.args.extra_height + mh) / 2 / np.sin(self.args.alpha * np.pi / 180 / 2) + cap_top_height
+        cr = (self.args.extra_height + mh) / 2 / np.sin(self.args.beta * np.pi / 180 / 2) + cap_top_height
+
+        shape = rotate_around_origin(shape, [0., 0., rr], row * self.args.alpha, [1., 0., 0.])
+        shape = rotate_around_origin(shape, [0., 0., cr], col * self.args.beta, [0., 1., 0.])
+
+        return shape
+
+    def get_spherical_shell(self):
+        cap_top_height = self.args.plate_thickness + self.args.key_height
+        mh = self.args.keyswitch_height + 3.0
+
+        rr = (self.args.extra_height + mh) / 2 / np.sin(self.args.alpha * np.pi / 180 / 2) + cap_top_height
+
+        shell = Translate([0., 0., rr])(Sphere(rr + 0.2 * self.args.plate_thickness, segments=50) - Sphere(rr - 0.8 * self.args.plate_thickness, segments=50))
+
+        limit_box = Translate([-70., -10., -20])(Cube([80., 90., 100.]))
+
+        cut_shell = shell * limit_box
+
+        return cut_shell
 
 
     def get_model(self):
 
-        return self.single_keyhole()
+        shell = self.get_spherical_shell()
+
+        key_holes = []
+        for i in range(-2, self.args.nrows-2):
+            for j in range(-2, self.args.ncols-2):
+                key_holes.append(self.transform_switch(self.single_keyhole(), i, j))
+
+                shell = shell - self.transform_switch(self.switch_cutout(), i, j)
+
+        return sum(key_holes)
 
 
     def to_scad(self, model=None, fname=None):
@@ -87,6 +137,35 @@ class Keyboard():
                                help='Thickness of retention tabs')
         parser.add_argument('--create-side-nubs', default=1, type=int,
                                help='Create side nubs for the key holes')
+
+        parser.add_argument('--alpha', default=10, type=float,
+                               help='Row angle')
+        parser.add_argument('--beta', default=10, type=float,
+                               help='Column angle')
+
+        parser.add_argument('--key-height', default=12.7, type=float,
+                               help='Key height')
+        parser.add_argument('--extra-height', default=1.0, type=float,
+                               help='Extra height for key spacing')
+
+        parser.add_argument('--nrows', default=4, type=int,
+                               help='Create side nubs for the key holes')
+        parser.add_argument('--ncols', default=4, type=int,
+                               help='Create side nubs for the key holes')
+
+
+parser = argparse.ArgumentParser()
+
+Keyboard.add_args(parser)
+
+args = parser.parse_args([])
+
+kb = Keyboard(args)
+
+model = kb.get_model()
+
+kb.to_scad(model=model, fname='things/model.scad')
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -96,6 +175,6 @@ if __name__ == "__main__":
 
     kb = Keyboard(args)
 
-    keyhole = kb.single_keyhole()
+    model = kb.get_model()
 
-    kb.to_scad(model=keyhole, fname='things/test.scad')
+    kb.to_scad(model=model, fname='things/model.scad')
---
+++ solid_update.py
@@ -0,0 +1,208 @@
+from solid import union, difference, intersection, hull
+from solid import cube, sphere, cylinder
+from solid import translate, mirror, scale, rotate
+import numpy as np
+
+
+class Cube(cube):
+
+    def __init__(self, size, center=False):
+        super().__init__(size, center=center)
+        size = np.array(size)
+        self.points = np.array([
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 0.0, 1.0],
+            [1.0, 1.0, 0.0],
+            [0.0, 1.0, 1.0],
+            [1.0, 0.0, 1.0],
+            [1.0, 1.0, 1.0],
+        ]) * size.reshape((1, -1))
+
+        if center:
+            self.points -= 0.5 * size.reshape((1, -1))
+
+    def get_points(self):
+        return self.points
+
+    def is_in(self, points):
+        #TODO: return an array of equal size to points with True or False
+        pass
+
+
+#TODO: expand to the full definition:
+class Cylinder(cylinder):
+
+    def __init__(self, h, r, center=False, segments=None):
+        super().__init__(h=h, r=r, center=center, segments=segments)
+        self.points = np.array([
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, h],
+        ])
+        #TODO: add segments around the perimiters, in "number of segments", as given
+
+        if center:
+            self.points -= 0.5 * np.array([[0., 0., h]])
+
+    def get_points(self):
+        return self.points
+
+
+#TODO: expand to full def:
+class Sphere(sphere):
+
+    #TODO: add segments around the perimiters, in "number of segments", as given
+    def __init__(self, r, segments=None):
+        super().__init__(r=r, segments=segments)
+        self.points = np.array([
+            [0.0, 0.0, 0.0],
+        ])
+
+    def get_points(self):
+        return self.points
+
+# TODO: expand functionality to full openscad style:
+class Rotate(rotate):
+
+    def __init__(self, a, v):
+        """Generate a rotation
+        Args:
+            a: angle (degrees)
+            v: vector around which to rotate
+        """
+        super().__init__(a=a, v=v)
+
+        self.rotation_matrix = rotation_matrix(v, a * np.pi / 180.)
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            points.append(np.einsum('ji,dj->di', self.rotation_matrix, child.get_points()))
+        return np.concatenate(points, axis=0)
+
+
+# TODO: expand functionality to full openscad style:
+class Translate(translate):
+
+    def __init__(self, v):
+        """Generate a translation
+        Args:
+            v: vector of the translation
+        """
+        super().__init__(v=v)
+
+        self.v = np.array(v)
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            points.append(child.get_points() + self.v.reshape((1,3)))
+        return np.concatenate(points, axis=0)
+
+class Scale(scale):
+
+    def __init__(self, v):
+        """Generate a scaling
+        Args:
+            v: scale parameters
+        """
+        super().__init__(v=v)
+
+        self.v = np.array(v)
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            points.append(child.get_points() * self.v.reshape((1,3)))
+        return np.concatenate(points, axis=0)
+
+class Mirror(mirror):
+
+    def __init__(self, v):
+        """Generate a mirror about a plane through the origin with normal v
+        Args:
+            v: normal vector
+        """
+        super().__init__(v=v)
+
+        self.v = np.array(v)
+        self.v_norm = self.v / np.linalg.norm(self.v)
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            child_points = child.get_points()
+            #project onto normal vector:
+            projections = np.dot(child_points, self.v_norm).reshape((-1,1)) * self.v_norm.reshape((1, 3))
+            #and subtract twice to get mirror
+            points.append(child_points - 2 * projections)
+        return np.concatenate(points, axis=0)
+
+class Union(union):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            points.append(child.get_points())
+        return np.concatenate(points, axis=0)
+
+class Intersection(intersection):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            points.append(child.get_points())
+        return np.concatenate(points, axis=0)
+
+class Difference(difference):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            points.append(child.get_points())
+        return np.concatenate(points, axis=0)
+
+class Hull(hull):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_points(self):
+        points = []
+        for child in self.children:
+            points.append(child.get_points())
+        return np.concatenate(points, axis=0)
+
+def rotation_matrix(axis, theta):
+    axis = axis / np.linalg.norm(axis)
+    rot = np.zeros((3,3))
+
+    ux, uy, uz = axis
+    s = np.sin(theta)
+    c = np.cos(theta)
+    mc = (1 - c)
+
+    rot[0, 0] = c + (ux ** 2) * mc
+    rot[1, 0] = uy * ux * mc + uz * s
+    rot[2, 0] = uz * ux * mc - uy * s
+
+
+    rot[0, 1] = ux * uy * mc - uz * s
+    rot[1, 1] = c + (uy ** 2) * mc
+    rot[2, 1] = uz * uy * mc + ux * s
+
+    rot[0, 2] = ux * uz * mc + uy * s
+    rot[1, 2] = uy * uz * mc - ux * s
+    rot[2, 2] = c + (uz ** 2) * mc
+
+    return rot
